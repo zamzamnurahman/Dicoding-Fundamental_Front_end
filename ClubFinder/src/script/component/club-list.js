2dc048aecdb97f692e932a81f0850c98f042bc63
zamzamnurahman <kazamnn1912@gmail.com>
2022-11-03 22:24:44 +0700

    webpack stack

--- ClubFinder/src/script/component/club-list.js
+++ ClubFinder/src/script/component/club-list.js
@@ -1,22 +1,38 @@
 import "./club-item.js";
 
 class ClubList extends HTMLElement {
+  constructor() {
+    super();
+    this.ShadowDOM = this.attachShadow({ mode: "open" });
+  }
+
   set clubs(clubs) {
     this._clubs = clubs;
     this.render();
   }
 
   render() {
-    this.innerHTML = "";
+    this.ShadowDOM.innerHTML = "";
     this._clubs.forEach((club) => {
       const clubItemElement = document.createElement("club-item");
       clubItemElement.club = club;
-      this.appendChild(clubItemElement);
+      this.ShadowDOM.appendChild(clubItemElement);
     });
   }
   renderError(message) {
-    this.innerHTML = "";
-    this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    this.ShadowDOM.innerHTML = `
+    <style>
+    .placeholder {
+      font-weight: lighter;
+      color: rgba(0, 0, 0, 0.5);
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+    </style>
+    `;
+    this.ShadowDOM.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   }
 }
 
